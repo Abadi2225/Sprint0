a9a368a552170e0f9e0f210e6bb746ed5c99d0e2
Abadi2225 <aefelemban@gmail.com>
2026-02-21 20:13:35 -0500

    Added Fireballs for Aquamentus

--- Jesse/Sprint2/obj/Debug/net9.0/Sprint.AssemblyInfo.cs
+++ Jesse/Sprint2/obj/Debug/net9.0/Sprint.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sprint")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9feb6e86de84db3256d2a7ad47303009ca68d24")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ade3e2f54f1c1ee81677a4590ab328d92560a9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sprint")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sprint")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
