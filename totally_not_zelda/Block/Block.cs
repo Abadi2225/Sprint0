0f9f1b7563ef5d3632ecf6e1ae2e235280a9ec15
Yifei Yu <adambi0503@gmail.com>
2026-03-06 18:00:14 -0500

    Implement basic collision

--- totally_not_zelda/Block/Block.cs
+++ totally_not_zelda/Block/Block.cs
@@ -8,28 +8,28 @@ namespace Sprint.Block;
 
 public class Block
 {
-    internal int tileWidth = (int)(16 * GameServices.ScaleFactor);
-    private readonly StaticSprite sprite;
-    public readonly bool walkAble;
-    private readonly Vector2 position;
-    public Rectangle Rect => new Rectangle((int)position.X, (int)position.Y, tileWidth, tileWidth);
+	internal int tileWidth = (int)(16 * GameServices.ScaleFactor);
+	private readonly StaticSprite sprite;
+	public readonly bool walkAble;
+	private readonly Vector2 position;
+	public Rectangle Rect => new Rectangle((int)position.X, (int)position.Y, tileWidth, tileWidth);
 	public Block(Texture2D texture, Vector2 pos, Rectangle sourceRect, bool walkable)
-    {
-        sprite = new StaticSprite(texture, pos, sourceRect);
-        this.walkAble = walkable;
-        position = pos;
-    }
+	{
+		sprite = new StaticSprite(texture, pos, sourceRect);
+		this.walkAble = walkable;
+		position = pos;
+	}
 
-    public void Draw(SpriteBatch spriteBatch)
-    {
-        sprite.Draw(spriteBatch, position);
-    }
+	public void Draw(SpriteBatch spriteBatch)
+	{
+		sprite.Draw(spriteBatch, position);
+	}
 
-    public void Update(GameTime time)
-    {
-        if (walkAble)
-        {
-            // Maybe add collision?
-        }
-    }
+	public void Update(GameTime time)
+	{
+		if (walkAble)
+		{
+			// Maybe add collision?
+		}
+	}
 }
---
+++ totally_not_zelda/Collisions/CollisionManager.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Xna.Framework;
+using Sprint.Block;
+using Sprint.Character;
+using Sprint.Enemies;
+using Sprint.Enemies.Base;
+using Sprint.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sprint.Collision
+{
+	internal class CollisionManager
+	{
+
+		public CollisionManager()
+		{
+
+		}
+		public void allCollisions(Link link, BlockManager blockManager, EnemyManager enemyManager)
+		{
+			LinkHitBlocks(link, blockManager);
+			LinkHitEnemy(link, enemyManager);
+		}
+
+		private void LinkHitBlocks(Link link, BlockManager blockManager)
+		{
+
+			foreach (var block in blockManager.blocksList)
+			{
+				if (block.walkAble)
+					continue;
+
+				if (!link.Rect.Intersects(block.Rect))
+					continue;
+
+				Rectangle overlap = Rectangle.Intersect(link.Rect, block.Rect);
+
+				if (overlap.Width < overlap.Height)
+				{
+					if (link.Rect.Center.X < block.Rect.Center.X)
+						link.Position -= new Vector2(overlap.Width, 0);
+					else
+						link.Position += new Vector2(overlap.Width, 0);
+				}
+				else
+				{
+					if (link.Rect.Center.Y < block.Rect.Center.Y)
+						link.Position -= new Vector2(0, overlap.Height);
+					else
+						link.Position += new Vector2(0, overlap.Height);
+				}
+			}
+		}
+
+		private void LinkHitEnemy(Link link, EnemyManager enemyManager)
+		{
+			foreach (var enemy in enemyManager.enemyList)
+			{
+				if (!link.Rect.Intersects(enemy.Rect))
+					continue;
+
+				Rectangle overlap = Rectangle.Intersect(link.Rect, enemy.Rect);
+
+				if (overlap.Width < overlap.Height)
+				{
+					if (link.Rect.Center.X < enemy.Rect.Center.X)
+						link.Position -= new Vector2(overlap.Width, 0);
+					else
+						link.Position += new Vector2(overlap.Width, 0);
+				}
+				else
+				{
+					if (link.Rect.Center.Y < enemy.Rect.Center.Y)
+						link.Position -= new Vector2(0, overlap.Height);
+					else
+						link.Position += new Vector2(0, overlap.Height);
+				}
+			}
+		}
+	}
+}
