bf7c36248007943dbe0adb53921aad72f6bceb4a
leaf09986 <leaf09986@gmail.com>
2026-03-06 23:08:07 -0500

    add cycling levels functionality with MB1 and MB2, add 1 more level

--- totally_not_zelda/Levels/LevelLoader.cs
+++ totally_not_zelda/Levels/LevelLoader.cs
@@ -1,10 +1,38 @@
 using System.IO;
 using System.Text.Json;
+using System.Collections.Generic;
 using Sprint.Levels;
 using Microsoft.Xna.Framework;
 
 public class LevelLoader
 {
+    // todo remove this
+    private List<string> levels = new List<string>{
+        "test_room",
+        "room2"
+    };
+    private int currentLevel = 0;
+    public LevelData CycleNext()
+    {
+        if (currentLevel < levels.Count - 1)
+        {
+            currentLevel++;
+        }
+        return Load(levels[currentLevel]);
+    }
+    public LevelData CyclePrevious()
+    {
+        if (currentLevel > 0)
+        {
+            currentLevel--;
+        }
+        return Load(levels[currentLevel]);
+    }
+    public LevelData GetCurrentLevel()
+    {
+        return Load(levels[currentLevel]);
+    }
+
     public LevelData Load(string levelName)
     {
         string path = $"Content/rooms/{levelName}.json";
