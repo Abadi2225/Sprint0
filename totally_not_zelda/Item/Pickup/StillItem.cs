ae8b41c845924a56d745b0845cbe22225c0f2c5e
Richard Cooper <richcoop2004@gmail.com>
2026-03-13 16:25:09 -0400

    Fixed item collision detection issue.

--- totally_not_zelda/Item/Pickup/StillItem.cs
+++ totally_not_zelda/Item/Pickup/StillItem.cs
@@ -10,8 +10,10 @@ internal class StillItem : AbstractItem
     public StillItem(string name, Texture2D texture, Vector2 pos, Rectangle sourceRect, float scale) : base(name, texture, pos)
     {
         sprite = new StaticSprite(texture, Position, sourceRect, scale);
+
+        // removed GameServices.ScaleFactor from multiplication steps
         Rect = new Rectangle((int)Position.X, (int)Position.Y,
-                (int)(sourceRect.Width * scale * GameServices.ScaleFactor),
-                (int)(sourceRect.Height * scale * GameServices.ScaleFactor));
+                (int)(sourceRect.Width * scale),
+                (int)(sourceRect.Height * scale));
     }
 }
