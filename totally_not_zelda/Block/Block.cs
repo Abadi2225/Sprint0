dc286b520d6637495cf1f1cf67c91c7624324c67
leaf09986 <leaf09986@gmail.com>
2026-04-23 19:35:31 -0400

    add parameter to change color mask of tiles

--- totally_not_zelda/Block/Block.cs
+++ totally_not_zelda/Block/Block.cs
@@ -8,34 +8,34 @@ namespace Sprint.Block;
 
 public class Block
 {
-	internal int tileWidth = (int)(16 * GameServices.ScaleFactor);
-	private readonly StaticSprite sprite;
-	public readonly bool walkAble;
-	public Vector2 Position;
-	public readonly bool pushAble;
-	public readonly bool IsStair;
-	public bool HasBeenPushed { get; set; } = false;
-	public Rectangle Rect => new Rectangle((int)Position.X, (int)Position.Y, tileWidth, tileWidth);
+    internal int tileWidth = (int)(16 * GameServices.ScaleFactor);
+    private readonly StaticSprite sprite;
+    public readonly bool walkAble;
+    public Vector2 Position;
+    public readonly bool pushAble;
+    public readonly bool IsStair;
+    public bool HasBeenPushed { get; set; } = false;
+    public Rectangle Rect => new Rectangle((int)Position.X, (int)Position.Y, tileWidth, tileWidth);
 
-	public Block(Texture2D texture, Vector2 pos, Rectangle sourceRect, bool walkable, bool pushable,  bool isStair = false)
-	{
-		if (texture != null)
-        	sprite = new StaticSprite(texture, pos, sourceRect);
-		this.walkAble = walkable;
-		this.Position = pos;
-		this.pushAble = pushable;
-		this.IsStair = isStair;
-	}
+    public Block(Texture2D texture, Vector2 pos, Rectangle sourceRect, uint colorMask, bool walkable, bool pushable, bool isStair = false)
+    {
+        if (texture != null)
+            sprite = new StaticSprite(texture, pos, sourceRect, colorMask);
+        this.walkAble = walkable;
+        this.Position = pos;
+        this.pushAble = pushable;
+        this.IsStair = isStair;
+    }
 
-	public void Draw(SpriteBatch spriteBatch)
-	{
-		if (sprite == null) return;
-    		sprite.Draw(spriteBatch, Position);
-	}
+    public void Draw(SpriteBatch spriteBatch)
+    {
+        if (sprite == null) return;
+        sprite.Draw(spriteBatch, Position);
+    }
 
-	public void Update(GameTime time)
-	{
+    public void Update(GameTime time)
+    {
 
-	}
+    }
 
 }
